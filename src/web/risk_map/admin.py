d82d9ce35b8687d9292f5ba99a4e62d4aef6e9ac
Modou Khabane Mbaye <modou.khabane.mbaye@usherbrooke.ca>
2026-06-27 02:28:14 -0400

    fix: enregistrer les modeles dans l admin Django

--- src/web/risk_map/admin.py
+++ src/web/risk_map/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import ElectricNetwork, FloodZone, RiskAnalysis, Alerte
 
-# Register your models here.
+
+@admin.register(ElectricNetwork)
+class ElectricNetworkAdmin(admin.ModelAdmin):
+    list_display = ["id", "osm_id", "type", "voltage", "criticality"]
+    list_filter  = ["type", "criticality"]
+    search_fields = ["osm_id", "type"]
+
+
+@admin.register(FloodZone)
+class FloodZoneAdmin(admin.ModelAdmin):
+    list_display = ["id", "source", "date_detection", "surface_ha"]
+    list_filter  = ["source"]
+
+
+@admin.register(RiskAnalysis)
+class RiskAnalysisAdmin(admin.ModelAdmin):
+    list_display = ["id", "infra_id", "niveau_risque", "distance_m", "date_analyse"]
+    list_filter  = ["niveau_risque"]
+
+
+@admin.register(Alerte)
+class AlerteAdmin(admin.ModelAdmin):
+    list_display = ["id", "niveau", "message", "date_alerte", "acquittee"]
+    list_filter  = ["niveau", "acquittee"]
+    actions      = ["marquer_acquittee"]
+
+    def marquer_acquittee(self, request, queryset):
+        queryset.update(acquittee=True)
+    marquer_acquittee.short_description = "Marquer comme acquittée"
